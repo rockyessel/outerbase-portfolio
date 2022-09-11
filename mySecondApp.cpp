6e9f44cdb1f5cbb7cb354da63a8c1dacb3f6d80b
Eoin Carrick <22carri99@gmail.com>
2022-09-11 21:33:24 +0000

    Add project files.

---
+++ mySecondApp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>;
+
+int main() {
+
+	int numberArr[] = { 1,2,4,6,7,8,6,5,4,6,8,4,6 };
+	int num = sizeof(numberArr);
+
+	std::cout << num;
+	
+	int x;
+	std::cout << "Enter number here ";
+	std::cin >> x;
+	std::cout << x;
+
+
+
+	system("pause>0");
+
+	return 0;
+
+};
